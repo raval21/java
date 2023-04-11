2743290ab4ae0213a2eacf2ad9a0d175099cf80d
Raj <Raj@DESKTOP-J7QQ2UP.mshome.net>
2023-04-11 00:28:49 -0700

    overloading

---
+++ methodoverloading/src/methodoverloading/overloading.java
@@ -0,0 +1,25 @@
+package methodoverloading;
+
+public class overloading{
+	static int add(int a, int b) 
+    { 
+      return a + b; 
+    }
+  
+    static int add(int a, int b, int c)
+    {
+        return a + b + c;
+    }
+  
+    public static void main(String args[])
+    {
+         
+        System.out.println(add(4, 6));
+        
+         
+        System.out.println(add(4, 6, 7));
+    }
+		
+	
+
+}
